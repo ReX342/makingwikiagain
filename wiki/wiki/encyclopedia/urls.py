de35392812e91e2734af022a8001ae1943b532a1
ReX342 <occ.rex@gmail.com>
2021-11-25 05:27:12 +0100

    askforfix

--- wiki/wiki/encyclopedia/urls.py
+++ wiki/wiki/encyclopedia/urls.py
@@ -5,5 +5,5 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("view/", views.view, name="view"),
-    path("<str:title>", views.detailv, name="detail view"),
+    path("<str:title>", views.detailv, name="detailview"),
 ]
