83309ef5c7cbfa0f624d1f9522c3e33d607c01ff
ReX342 <occ.rex@gmail.com>
2021-11-25 04:18:13 +0100

    no parenthesis after quotationmark syntax

--- wiki/wiki/encyclopedia/forms.py
+++ wiki/wiki/encyclopedia/forms.py
@@ -1,6 +1,25 @@
 from django import forms
 
 class PageForm(forms.Form):
-    title = forms.CharField(max_length=100)
-    content = forms.CharField(widget=forms.Textarea)
+    title = forms.CharField(
+        max_length=100,
+        widget=forms.TextInput(
+            attrs={
+                'class': 'form-control mb-2',
+                'placeholder': 'Title here'
+            }
+        )
+    )
+    content = forms.CharField(max_length=10240,
+        widget=forms.Textarea(attrs={
+            'class': 'form-control mb-2', 
+            'rows': '10', 
+            'placeholder': 'Write your content here'
+        })
     nope = forms.BooleanField(required=False)
+    def clean(self):
+        cleaned_data = super(PagegForm, self).clean()
+    title = cleaned_data.get('title')
+    content = cleaned_data.get('content')
+    if nope:
+        raise forms.ValidationError('Not allowed to make this entry!')
--- wiki/wiki/encyclopedia/urls.py
+++ wiki/wiki/encyclopedia/urls.py
@@ -1,9 +1,18 @@
-from django.urls import path
+# from django.http import HttpResponse
+# from django.http import HttpResponseNotFound
+# from django.http import HttpResponseRedirect
+# from django.shortcuts import render, reverse
+# from django.contrib import messages
+
+# import markdown
+# from . import util
+# from . import forms
+# None of this helps to gget detail view working
 
 from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
     path("view/", views.view, name="view"),
-    path("<str:title>"), views.detailv, name="detail view"),
+    path("<str:title>", views.detailv, name="detail view"),
 ]
